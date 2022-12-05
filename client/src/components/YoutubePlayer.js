c1ae8e0007049d182528172c995859d5951f942a
Thomas Aloi <Tom Aloi@DESKTOP-RSAFK29>
2022-12-04 19:26:55 -0500

    Working playlist buttons)

--- client/src/components/YoutubePlayer.js
+++ client/src/components/YoutubePlayer.js
@@ -4,10 +4,17 @@ import React from 'react';
 import YouTube from 'react-youtube';
 import { Box } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import StopIcon from '@mui/icons-material/Stop';
+import FastForwardIcon from '@mui/icons-material/FastForward';
+import FastRewindIcon from '@mui/icons-material/FastRewind';
 
 export default function YouTubePlayer() {
     const { store } = useContext(GlobalStoreContext);
     const [listData, setListData] = useState({name: "", songNum: "", title: "", artist: ""});
+    const [player, setPlayer] = useState(null);
+    const [currentSong, setCurrentSong] = useState(0);
     // THIS EXAMPLE DEMONSTRATES HOW TO DYNAMICALLY MAKE A
     // YOUTUBE PLAYER AND EMBED IT IN YOUR SITE. IT ALSO
     // DEMONSTRATES HOW TO IMPLEMENT A PLAYLIST THAT MOVES
@@ -23,7 +30,6 @@ export default function YouTubePlayer() {
     }
 
     // THIS IS THE INDEX OF THE SONG CURRENTLY IN USE IN THE PLAYLIST
-    let currentSong = 0;
 
     const playerOptions = {
         height: '390',
@@ -50,8 +56,18 @@ export default function YouTubePlayer() {
 
     // THIS FUNCTION INCREMENTS THE PLAYLIST SONG TO THE NEXT ONE
     function incSong() {
-        currentSong++;
-        currentSong = currentSong % playlist.length;
+        setCurrentSong(currentSong + 1);
+        if (currentSong >= playlist.length) {
+            setCurrentSong(0);
+        }
+    }
+
+    function decSong() {
+        setCurrentSong(currentSong - 1);
+        console.log(currentSong);
+        if (currentSong <= 0) {
+            setCurrentSong(playlist.length - 1);
+        }
     }
 
     function onPlayerReady(event) {
@@ -65,7 +81,11 @@ export default function YouTubePlayer() {
     // VALUE OF 0 MEANS THE SONG PLAYING HAS ENDED.
     function onPlayerStateChange(event) {
         let playerStatus = event.data;
-        let player = event.target;
+        setPlayer(event.target);
+        playerStateChange(playerStatus)
+    }
+
+    function playerStateChange(playerStatus) {
         if (playerStatus === -1) {
             // VIDEO UNSTARTED
             console.log("-1 Video unstarted");
@@ -89,6 +109,28 @@ export default function YouTubePlayer() {
         }
     }
 
+    function handleBack () {
+        decSong();
+        let song = playlist[currentSong];
+        player.loadVideoById(song);
+        player.playVideo();
+        setListData({
+            name: store.currentList.name,
+            songNum: currentSong+1,
+            title: store.currentList.songs[currentSong].title,
+            artist: store.currentList.songs[currentSong].artist
+        })
+    }
+    function handlePause () {
+        player.pauseVideo();
+    }
+    function handlePlay () {
+        player.playVideo();
+    }
+    function handleForward () {
+        playerStateChange(0);
+    }
+
     let songInfo = <Box style={{left:'5%', position:'absolute'}}> 
         <Typography style={{fontWeight:'bold'}}>Playlist: {' '} {listData.name}</Typography> 
         <Typography style={{fontWeight:'bold'}}>Song #: {' '} {listData.songNum}</Typography> 
@@ -105,6 +147,29 @@ export default function YouTubePlayer() {
             onStateChange={onPlayerStateChange} />
         
             {songInfo}
+
+            <Box sx={{position: 'absolute', bottom: '0%', left: '0%', justifyContent: 'center', display: 'flex', width:'100%', height: 'auto'}}>
+                    <IconButton 
+                    onClick={handleBack} 
+                    aria-label='back'>
+                        <FastRewindIcon style={{fontSize:'26pt', flex: 'none'}} sx={{color:'ffffff'}} />
+                    </IconButton>
+                    <IconButton 
+                    onClick={handlePause} 
+                    aria-label='pause'>
+                        <StopIcon style={{fontSize:'26pt', flex: 'none'}} sx={{color:'ffffff'}} />
+                    </IconButton>
+                    <IconButton 
+                    onClick={handlePlay} 
+                    aria-label='play'>
+                        <PlayArrowIcon style={{fontSize:'26pt', flex: 'none'}} sx={{color:'ffffff'}} />
+                    </IconButton>
+                    <IconButton 
+                    onClick={handleForward} 
+                    aria-label='forward'>
+                        <FastForwardIcon style={{fontSize:'26pt', flex: 'none'}} sx={{color:'ffffff'}} />
+                    </IconButton>
+                </Box>
         </div>;
     } else {
         return "";
