dce44d21ad947d2724578426cf8b7b566be9f034
Thomas Aloi <Tom Aloi@DESKTOP-RSAFK29>
2022-12-06 17:35:06 -0500

    Fixed lists not staying closed

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -51,7 +51,8 @@ function ListCard(props) {
         store.setCurrentList(id);
     }
 
-    function handleCloseList() {
+    function handleCloseList(event) {
+        event.stopPropagation();
         store.closeCurrentList();
     }
 
